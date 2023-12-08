55b836076de7a86ec057bf7c60ac6661a665bfce
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-08 16:43:03 +0800

    feat: refactor code and add tests for improved functionality
    - Modify the `createTodo` factory function to include a default value for the `description` parameter
    - Create a new function `createPlayersAndUsers` that returns an object with player and user data
    - Modify the `addTodo` test to use the updated `createTodo` function with the title "eating"
    - Add a new test named "難以理解的 given" that tests the `fighting` function with player and user data
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- delegated.spec.ts
+++ delegated.spec.ts
@@ -1,21 +1,71 @@
 import { todos, addTodo } from "./index";
 
-const createTodo = (title: string) => {
+// 工廠函數
+const createTodo = (
+  title: string,
+  description: string = "today we go test"
+) => {
   return {
     title,
-    description: "today we go test",
+    description,
+  };
+};
+
+const createPlayersAndUsers = () => {
+  const userA = {
+    name: "A",
+    age: 18,
+  };
+
+  const userB = {
+    name: "B",
+    age: 18,
+  };
+
+  const userC = {
+    name: "C",
+    age: 18,
+  };
+
+  const playerA = new Player(new Weapons("sword"));
+  playerA.life = 3;
+  playerA.attack = () => {
+    return 3;
+  };
+
+  const playerB = new Player(new Weapons("gun"));
+  playerB.life = 2;
+  playerB.attack = () => {
+    return 3;
+  };
+
+  return {
+    playerA,
+    playerB,
+    users: [userA, userB, userC],
   };
 };
 
 describe("delegated", () => {
   it("addTodo", () => {
     // given
-    const todo = createTodo("test");
+    const todo = createTodo("eating");
 
     // when
     addTodo(todo);
 
     // then
     expect(todos[0]).toBe(todo);
+  });
+
+  it("難以理解的 given", () => {
+    // given
+    const { playerA, playerB, users } = createPlayersAndUsers();
+
+    // when
+    fighting(playerA, playerB, users);
+
+    // then
+    expect(playerA.life).toBe(2);
   });
 });
