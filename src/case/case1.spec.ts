50c99bfb2aa55140c45a391ae63fdbe492f11417
stevecyj <ycsteven.tsao@gmail.com>
2023-12-12 00:15:13 +0800

    test: refactor test cases in `case1.spec.ts`
    - Add import statement for `userAge` in `case1.spec.ts`
    - Comment out the `vi.mock` function for `userAge` in `case1.spec.ts`
    - Add a new `vi.mock` function for `userAge` in `case1.spec.ts`
    - Add a new test case for `doubleUserAge` in `case1.spec.ts`
    - Modify the return value of `userAge` in the first test case in `case1.spec.ts`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/case/case1.spec.ts
+++ src/case/case1.spec.ts
@@ -1,16 +1,31 @@
 import { doubleUserAge } from "./index";
+import { userAge } from "./user";
 import { vi, it, expect, describe } from "vitest";
 
 // stub, 替換掉真實的邏輯實現
-vi.mock("./user", () => {
-  return {
-    userAge: () => 2,
-  };
-});
+// vi.mock("./user", () => {
+//   return {
+//     userAge: () => 2,
+//   };
+// });
+
+vi.mock("./user");
 
 describe("間接 input", () => {
+  it("second", () => {
+    // given
+    vi.mocked(userAge).mockReturnValue(3);
+
+    // when
+    const actual = doubleUserAge();
+
+    // then
+    expect(actual).toBe(6);
+  });
+
   it("first", () => {
     // given
+    vi.mocked(userAge).mockReturnValue(2);
 
     // when
     const actual = doubleUserAge();
