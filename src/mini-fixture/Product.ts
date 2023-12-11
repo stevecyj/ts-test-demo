dcbf5b48df74d8d0e04bbd33b3b623ad405bbec1
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-11 12:13:17 +0800

    feat: add Product class to mini-fixture directory
    - Add a new file `Product.ts` in the `src/mini-fixture` directory
    - Define a `Product` class with properties `name`, `price`, and `description`
    - Implement a constructor for the `Product` class that takes in `name`, `price`, and `description` as parameters and assigns them to the corresponding properties
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ src/mini-fixture/Product.ts
@@ -0,0 +1,10 @@
+export class Product {
+  name: any;
+  price: any;
+  description: any;
+  constructor(name: string, price: number, description: string) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+}
