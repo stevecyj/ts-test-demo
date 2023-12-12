c7ba2d117518aff8d7fa9e562b3b63c4521b6d67
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-12 15:03:33 +0800

    feat: add mock API and third-party modules for case 2
    - Add a new file `mock/api.mock.ts`
    - Add a new file `src/case2/third-party-modules.spec.ts`
    - Add a new file `src/case2/third-party-modules.ts`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ src/case2/third-party-modules.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect, vi } from "vitest";
+import { doubleUserAge } from "./third-party-modules";
+import axios from "axios"; // Import the 'axios' module
+
+vi.mock("axios");
+
+test("第3方模塊處理 axios", async () => {
+  vi.mocked(axios).mockResolvedValue({ name: "JOJO", age: 18 });
+
+  const result = await doubleUserAge();
+
+  expect(result).toBe(36);
+});
---
+++ src/case2/third-party-modules.ts
@@ -0,0 +1,13 @@
+// 調用第3方模塊，比如調用了 axios，如何測試？
+import axios from "axios";
+
+interface User {
+  name: string;
+  age: number;
+}
+
+export async function doubleUserAge() {
+  const user: User = await axios("/api/test");
+  console.log("user =====> ", user);
+  return user.age * 2;
+}
