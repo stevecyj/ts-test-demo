63cdda9a7699286fb4bcb4770b714f37ed53abd1
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-12 16:13:15 +0800

    vi mock and mocked seperate

--- src/case1/case1.spec.ts
+++ src/case1/case1.spec.ts
@@ -1,47 +1,31 @@
-// import { doubleUserAge } from "./index";
+import { doubleUserAge } from "./index";
 import { userAge, fetchUserAge } from "./user";
 import { vi, it, expect, describe } from "vitest";
 
 // stub, 替換掉真實的邏輯實現
-vi.mock("./user", () => {
-  return {
-    userAge: () => 3,
-    fetchUserAge: () => Promise.resolve(6),
-  };
-});
+// vi.mock("./user", () => {
+//   return {
+//     userAge: () => 3,
+//     // fetchUserAge: () => Promise.resolve(6),
+//   };
+// });
 
-// vi.mock("./user");
+vi.mock("./user");
 
 describe("間接 input", () => {
-  // beforeEach(() => {
-  //   vi.doMock("./user", () => {
-  //     return {
-  //       userAge: () => 3,
-  //     };
-  //   });
-  // });
-
-  it.only("second", async () => {
+  it("first", async () => {
     // given
-    // vi.mocked(userAge).mockReturnValue(3);
-    // const { doubleUserAge } = await import("./index");
+    vi.mocked(userAge).mockReturnValue(8);
 
     // when
-    const actual = await fetchUserAge();
+    const actual = doubleUserAge();
 
     // then
-    expect(actual).toBe(6);
+    expect(actual).toBe(16);
   });
 
-  // it("first", async () => {
-  //   // given
-  //   vi.mocked(userAge).mockReturnValue(2);
-  //   const { doubleUserAge } = await import("./index");
-
-  //   // when
-  //   const actual = doubleUserAge();
-
-  //   // then
-  //   expect(actual).toBe(4);
-  // });
+  it("second", async () => {
+    vi.mocked(userAge).mockReturnValue(12);
+    console.log("second userAge", userAge());
+  });
 });
