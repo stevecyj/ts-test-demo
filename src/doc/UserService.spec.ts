05b43639262b7adf31555f4f30fd10378f039922
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-18 10:01:36 +0800

    test: refactor UserService to improve test coverage
    - Add `vi` import to UserService.spec.ts
    - Add behavior verification test for adding user to database in UserService.spec.ts
    - Modify createUser method in UserService.ts to include behavior verification comment
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/doc/UserService.spec.ts
+++ src/doc/UserService.spec.ts
@@ -1,8 +1,9 @@
-import { it, expect, describe } from "vitest";
+import { vi, it, expect, describe } from "vitest";
 import { UserService } from "./UserService";
 import { Database } from "./database";
 
 describe("UserService", () => {
+  // 狀態驗證測試
   it("should create user", () => {
     const database = new Database();
     const userService = new UserService(database);
@@ -12,5 +13,19 @@ describe("UserService", () => {
     // key: 到 database 裡面找到 user.id 的資料，並且檢查 name 是否為 John
     // expect(database.getUser(user.id)?.name).toBe("John");
     expect(userService.findUser(user.id)?.name).toBe("John");
+  });
+
+  // 行為驗證測試
+  it.only("should add user to database", () => {
+    Database.prototype.addUser = vi.fn();
+    const database = new Database();
+    // vi.spyOn(database, "addUser");
+    console.log("addUser", database.addUser);
+
+    const userService = new UserService(database);
+
+    userService.createUser("John");
+
+    expect(database.addUser).toBeCalled();
   });
 });
--- src/doc/UserService.ts
+++ src/doc/UserService.ts
@@ -7,7 +7,7 @@ export class UserService {
   createUser(name: string): IUser {
     const id = Math.floor(Math.random() * 1000);
     const newUser: IUser = { id, name };
-    this.db.addUser(newUser);
+    this.db.addUser(newUser); // 行為驗證測這裡
     return newUser;
   }
 
