436a029f491e469909e572540f89335b740ec876
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-20 15:03:26 +0800

    refactor: refactor code and improve timeout behavior
    - Remove commented out code
    - Modify timeout behavior to advance timers to next timer twice
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/setTimeout/setTimeout.spec.ts
+++ src/setTimeout/setTimeout.spec.ts
@@ -4,12 +4,15 @@ import { User } from "./setTimeout";
 describe("setTimeout", () => {
   it("should fetch User data", () => {
     vi.useFakeTimers();
+
     const user = new User("1");
     const delay = 1000;
     const callback = vi.fn();
     user.fetchData(callback, delay);
     // vi.advanceTimersByTime(delay); // Advance time by 1000ms
-    vi.advanceTimersToNextTimer(); // Advance time to next timer
+    // vi.advanceTimersToNextTimer(); // Advance time to next timer
+
+    vi.runAllTimers(); // Advance time to next timer
 
     expect(callback).toBeCalledWith(`Data of user 1`);
   });
