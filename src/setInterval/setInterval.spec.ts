8fb05754e6fdd1cdfb4bd8a5ee65a85d04eae733
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-20 15:21:58 +0800

    feat: add setInterval functionality with test case and faked timers
    - Add new file `setInterval.spec.ts` to the `src/setInterval` directory
    - Define a test case in `setInterval.spec.ts` for the `sayHi` function
    - Use `vi` to fake timers and spy on `console.log` in the test case
    - Call the `sayHi` function and advance timers to the next timer
    - Expect `console.log` to be called with the string "hi"
    - Add new file `setInterval.ts` to the `src/setInterval` directory
    - Define the `sayHi` function in `setInterval.ts`
    - Use `setInterval` to log "hi" to the console every 1000ms
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ src/setInterval/setInterval.spec.ts
@@ -0,0 +1,14 @@
+import { it, expect, describe, vi } from "vitest";
+import { sayHi } from "./setInterval";
+
+describe("setInterval", () => {
+  it("should call one", () => {
+    vi.useFakeTimers();
+    vi.spyOn(console, "log");
+    sayHi();
+    // vi.advanceTimersByTime(1000); // Advance time by 1000ms
+    vi.advanceTimersToNextTimer(); // Advance time to next timer
+
+    expect(console.log).toBeCalledWith("hi");
+  });
+});
---
+++ src/setInterval/setInterval.ts
@@ -0,0 +1,5 @@
+export function sayHi() {
+  setInterval(() => {
+    console.log("hi");
+  }, 1000);
+}
